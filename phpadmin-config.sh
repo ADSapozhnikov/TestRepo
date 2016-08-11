35bedfac0b2aec0a7f3ca3333ea7e3a7c920b906
ADSapozhnikov <a.d.sapozhnikov@gmail.com>
2016-08-11 11:11:33 +0300

    Fist commit

---
+++ phpadmin-config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Variables
+DBPASSWD=123456
+sudo su
+sudo apt-get update && apt-get -y upgrade
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
+sudo apt-get -y install phpmyadmin
